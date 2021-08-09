9da810479ea7c25d80cea35a50a21ab857a87141
ayarmkt <ayat.0906@gmail.com>
2021-08-09 17:05:52 +0900

    added button

--- src/store/bug-slice.js
+++ src/store/bug-slice.js
@@ -1,6 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit';
 //import RetrieveData from '../components/RetrieveData';
 
+// const DUMMY_DATA = [
+//   {
+//     id: 23456789,
+//     name: 'Crashed on Load',
+//     details: 'Crashed after 3 seconds',
+//     steps: 'Open application and it will crash',
+//     version: 'V2.0',
+//     priority: 1,
+//     assigned: 'Aya',
+//     creator: 'Joe',
+//     time: '23:30',
+//   },
+//   {
+//     id: 23456786,
+//     name: 'cannot enter again',
+//     details: 'Crashed after 3 seconds',
+//     steps: 'Open application and it will crash',
+//     version: 'V2.0',
+//     priority: 5,
+//     assigned: 'Tom',
+//     creator: 'Joe',
+//     time: '23:38',
+//   },
+//   {
+//     id: 23456787,
+//     name: 'cannot enter ',
+//     details: 'Crashed after 3 seconds',
+//     steps: 'Open application and it will crash',
+//     version: 'V2.0',
+//     priority: 3,
+//     assigned: 'Aya',
+//     creator: 'Joe',
+//     time: '23:38',
+//   },
+// ];
+
 const bugSlice = createSlice({
   name: 'bug',
   initialState: [],
