d234291d7b4c68385f2afcebef766e8cad6301ee
ayarmkt <ayat.0906@gmail.com>
2021-08-10 17:33:22 +0900

    updated design

--- src/components/Bugs/AddNewBug.js
+++ src/components/Bugs/AddNewBug.js
@@ -36,39 +36,66 @@ const AddNewBug = () => {
   console.log(bugs);
 
   return (
-    <form onSubmit={addNewBugHandler}>
-      <div className={classes['add-bug-form']}>
-        <div className={classes['add-bug-input']}>
-          <label>Title</label>
-          <input type='text' ref={enteredTitle}></input>
+    <div className={classes.container}>
+      <h1>Create New Bug</h1>
+      <form className={classes['bug-form']} onSubmit={addNewBugHandler}>
+        <div className={classes['add-bug-form']}>
+          <div className={classes['add-bug-input']}>
+            <label>Title</label>
+            <input
+              type='text'
+              placeholder='enter title'
+              ref={enteredTitle}
+            ></input>
+          </div>
+          <div className={classes['add-bug-input']}>
+            <label>Details</label>
+            <input
+              type='text'
+              placeholder='enter details'
+              ref={enteredDetails}
+            ></input>
+          </div>
+          <div className={classes['add-bug-input']}>
+            <label>Steps</label>
+            <input
+              type='text'
+              placeholder='enter steps'
+              ref={enteredSteps}
+            ></input>
+          </div>
+          <div className={classes['add-bug-input']}>
+            <label>Version</label>
+            <input
+              type='text'
+              placeholder='version 2'
+              ref={enteredVersion}
+            ></input>
+          </div>
+          <div className={classes['add-bug-input']}>
+            <label>Priority</label>
+            <input type='number' placeholder='1' ref={enteredPriority}></input>
+          </div>
+          <div className={classes['add-bug-input']}>
+            <label>Assigned</label>
+            <input
+              type='text'
+              placeholder='assigned person here'
+              ref={enteredAssigned}
+            ></input>
+          </div>
+          <div className={classes['add-bug-input']}>
+            <label>Creator</label>
+            <input
+              type='text'
+              placeholder='creator name'
+              ref={enteredCreator}
+            ></input>
+          </div>
         </div>
-        <div className={classes['add-bug-input']}>
-          <label>Details</label>
-          <input type='text' ref={enteredDetails}></input>
-        </div>
-        <div className={classes['add-bug-input']}>
-          <label>Steps</label>
-          <input type='text' ref={enteredSteps}></input>
-        </div>
-        <div className={classes.input}>
-          <label>Version</label>
-          <input type='text' ref={enteredVersion}></input>
-        </div>
-        <div className={classes.input}>
-          <label>Priority</label>
-          <input type='number' ref={enteredPriority}></input>
-        </div>
-        <div className={classes.input}>
-          <label>Assigned</label>
-          <input type='text' ref={enteredAssigned}></input>
-        </div>
-        <div className={classes.input}>
-          <label>Creator</label>
-          <input type='text' ref={enteredCreator}></input>
-        </div>
-      </div>
-      <button type='submit'>Add new bug</button>
-    </form>
+        <button type='submit'>Add new bug</button>
+      </form>
+    </div>
   );
 };
 
