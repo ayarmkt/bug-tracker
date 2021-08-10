cfc602619f488dd6363cbc199409c3bafe8ba47b
ayarmkt <ayat.0906@gmail.com>
2021-08-10 22:11:13 +0900

    updated add new bug logic

--- src/components/Bugs/AddNewBug.js
+++ src/components/Bugs/AddNewBug.js
@@ -1,96 +1,158 @@
 import classes from './AddNewBug.module.css';
 import React from 'react';
-import { useRef } from 'react';
-//import { useDispatch } from 'react-redux';
-//import { createBugs } from '../../store/bug-slice';
+//import { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { addNewBugs } from '../../store/bug-slice';
 import { useSelector } from 'react-redux';
+import useAddNewBug from '../../hooks/useAddNewBug';
 
 const AddNewBug = () => {
-  const enteredTitle = useRef();
-  const enteredDetails = useRef();
-  const enteredSteps = useRef();
-  const enteredVersion = useRef();
-  const enteredPriority = useRef();
-  const enteredAssigned = useRef();
-  const enteredCreator = useRef();
+  const dispatch = useDispatch();
 
-  //   const newBug = {
-  //     enteredTitle,
-  //     enteredDetails,
-  //     enteredSteps,
-  //     enteredVersion,
-  //     enteredPriority,
-  //     enteredAssigned,
-  //     enteredCreator,
-  //   };
+  const {
+    enteredValue: enteredTitle,
+    valueChangeHandler: titleChangeHandler,
+    resetValue: resetTitle,
+  } = useAddNewBug();
 
-  //const dispatch = useDispatch();
+  const {
+    enteredValue: enteredDetails,
+    valueChangeHandler: detailsChangeHandler,
+    resetValue: resetDetails,
+  } = useAddNewBug();
 
-  const addNewBugHandler = () => {
+  const {
+    enteredValue: enteredSteps,
+    valueChangeHandler: stepsChangeHandler,
+    resetValue: resetSteps,
+  } = useAddNewBug();
+
+  const {
+    enteredValue: enteredVersion,
+    valueChangeHandler: versionChangeHandler,
+    resetValue: resetVersion,
+  } = useAddNewBug();
+
+  const {
+    enteredValue: enteredPriority,
+    valueChangeHandler: priorityChangeHandler,
+    resetValue: resetPriority,
+  } = useAddNewBug();
+
+  //const [enteredPriority, setEnteredPriority] = useState();
+
+  const {
+    enteredValue: enteredAssigned,
+    valueChangeHandler: assignedChangeHandler,
+    resetValue: resetAssigned,
+  } = useAddNewBug();
+
+  const {
+    enteredValue: enteredCreator,
+    valueChangeHandler: creatorChangeHandler,
+    resetValue: resetCreator,
+  } = useAddNewBug();
+
+  const submitNewBugHandler = (e) => {
+    e.preventDefault();
     console.log('added');
-    // dispatch(createBugs(newBug));
-    // console.log(newBug);
+
+    const enteredTime = new Date().getTime();
+    const enteredId = toString(enteredTime) + enteredTitle;
+
+    const newBug = {
+      title: enteredTitle,
+      details: enteredDetails,
+      steps: enteredSteps,
+      version: enteredVersion,
+      priority: enteredPriority,
+      assigned: enteredAssigned,
+      creator: enteredCreator,
+      time: enteredTime,
+      id: enteredId,
+    };
+    console.log(newBug);
+
+    dispatch(addNewBugs(newBug));
+    resetTitle();
+    resetDetails();
+    resetSteps();
+    resetVersion();
+    resetPriority();
+    resetAssigned();
+    resetCreator();
   };
 
-  const bugs = useSelector((state) => state.bugs);
+  const { bugs } = useSelector((state) => state.bugs);
   console.log(bugs);
 
   return (
     <div className={classes.container}>
       <h1>Create New Bug</h1>
-      <form className={classes['bug-form']} onSubmit={addNewBugHandler}>
+      <form className={classes['bug-form']} onSubmit={submitNewBugHandler}>
         <div className={classes['add-bug-form']}>
           <div className={classes['add-bug-input']}>
             <label>Title</label>
             <input
               type='text'
               placeholder='enter title'
-              ref={enteredTitle}
-            ></input>
+              onChange={titleChangeHandler}
+              value={enteredTitle}
+            />
           </div>
           <div className={classes['add-bug-input']}>
             <label>Details</label>
             <input
               type='text'
               placeholder='enter details'
-              ref={enteredDetails}
-            ></input>
+              onChange={detailsChangeHandler}
+              value={enteredDetails}
+            />
           </div>
           <div className={classes['add-bug-input']}>
             <label>Steps</label>
             <input
               type='text'
               placeholder='enter steps'
-              ref={enteredSteps}
-            ></input>
+              onChange={stepsChangeHandler}
+              value={enteredSteps}
+            />
           </div>
           <div className={classes['add-bug-input']}>
             <label>Version</label>
             <input
               type='text'
               placeholder='version 2'
-              ref={enteredVersion}
-            ></input>
+              onChange={versionChangeHandler}
+              value={enteredVersion}
+            />
           </div>
           <div className={classes['add-bug-input']}>
             <label>Priority</label>
-            <input type='number' placeholder='1' ref={enteredPriority}></input>
+            <input
+              type='number'
+              placeholder='1'
+              onChange={priorityChangeHandler}
+              value={enteredPriority}
+            />
           </div>
           <div className={classes['add-bug-input']}>
             <label>Assigned</label>
             <input
               type='text'
               placeholder='assigned person here'
-              ref={enteredAssigned}
-            ></input>
+              onChange={assignedChangeHandler}
+              value={enteredAssigned}
+            />
           </div>
           <div className={classes['add-bug-input']}>
             <label>Creator</label>
             <input
               type='text'
               placeholder='creator name'
-              ref={enteredCreator}
-            ></input>
+              onChange={creatorChangeHandler}
+              value={enteredCreator}
+            />
           </div>
         </div>
         <button type='submit'>Add new bug</button>
--- src/components/Bugs/BugsList.js
+++ src/components/Bugs/BugsList.js
@@ -1,64 +1,77 @@
 // import { useEffect } from 'react';
 import React from 'react';
 import classes from './BugsList.module.css';
-//import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+//import { useEffect } from 'react';
 //import { useDispatch } from 'react-redux';
 //import { getBugs } from '../../store/bug-slice';
 import BugItem from './BugItem';
 
-const DUMMY_DATA = [
-  {
-    id: 23456789,
-    title: 'Crashed on Load',
-    details: 'Crashed after 3 seconds',
-    steps: 'Open application and it will crash',
-    version: 'V2.0',
-    priority: 1,
-    assigned: 'Aya',
-    creator: 'Joe',
-    time: '23:30',
-  },
-  {
-    id: 23456786,
-    title: 'cannot enter again',
-    details: 'Crashed after 3 seconds',
-    steps: 'Open application and it will crash',
-    version: 'V2.0',
-    priority: 5,
-    assigned: 'Tom',
-    creator: 'Joe',
-    time: '23:38',
-  },
-  {
-    id: 23456787,
-    title: 'cannot enter ',
-    details: 'Crashed after 3 seconds',
-    steps: 'Open application and it will crash',
-    version: 'V2.0',
-    priority: 3,
-    assigned: 'Aya',
-    creator: 'Joe',
-    time: '23:38',
-  },
-];
+// const DUMMY_DATA = [
+//   {
+//     id: 23456789,
+//     title: 'Crashed on Load',
+//     details: 'Crashed after 3 seconds',
+//     steps: 'Open application and it will crash',
+//     version: 'V2.0',
+//     priority: 1,
+//     assigned: 'Aya',
+//     creator: 'Joe',
+//     time: '23:30',
+//   },
+//   {
+//     id: 23456786,
+//     title: 'cannot enter again',
+//     details: 'Crashed after 3 seconds',
+//     steps: 'Open application and it will crash',
+//     version: 'V2.0',
+//     priority: 5,
+//     assigned: 'Tom',
+//     creator: 'Joe',
+//     time: '23:38',
+//   },
+//   {
+//     id: 23456787,
+//     title: 'cannot enter ',
+//     details: 'Crashed after 3 seconds',
+//     steps: 'Open application and it will crash',
+//     version: 'V2.0',
+//     priority: 3,
+//     assigned: 'Aya',
+//     creator: 'Joe',
+//     time: '23:38',
+//   },
+// ];
 
 const BugsList = () => {
   //const dispatch = useDispatch();
+  //let sortedData;
 
-  //const bugs = useSelector((state) => state.bugs);
+  const { bugs } = useSelector((state) => state.bugs);
+  console.log(bugs);
 
-  let sortedData;
-
-  if (DUMMY_DATA.length > 1) {
+  let sortedArray = [...bugs];
+  console.log(sortedArray);
+  if (sortedArray.length > 1) {
     // sortedData = dispatch(getBugs(DUMMY_DATA));
-    sortedData = DUMMY_DATA.sort((a, b) => a.priority - b.priority);
-    // return sortedData;
-    console.log(sortedData);
+    sortedArray.sort((a, b) => Number(a.priority) - Number(b.priority));
   }
+  console.log(sortedArray);
 
   // useEffect(() => {
   //   dispatch(getBugs());
-  // }, []);
+  // }, [dispatch]);
+
+  // if (bugs.length > 1) {
+  //   // sortedData = dispatch(getBugs(DUMMY_DATA));
+  //   sortedData = bugs.sort((a, b) => a.priority - b.priority);
+  //   // return sortedData;
+  //   console.log(sortedData);
+  // } else {
+  //   sortedData = bugs;
+  // }
+
+  //console.log(sortedData);
 
   return (
     <div className={classes.container}>
@@ -72,14 +85,18 @@ const BugsList = () => {
           <p>Assigned</p>
           <p>Creator</p>
         </li>
-        {sortedData.map((bug) => (
-          <BugItem
-            className={classes.items}
-            bug={bug}
-            key={bug.id}
-            id={bug.id}
-          />
-        ))}
+        {sortedArray.length >= 1 &&
+          sortedArray.map((bug) => (
+            <BugItem
+              className={classes.items}
+              bug={bug}
+              key={bug.id}
+              id={bug.id}
+            />
+          ))}
+        {sortedArray.length === 0 && (
+          <p className={classes.error}>No bugs found.</p>
+        )}
       </ul>
     </div>
   );
