f91e3f91f07bbe28191e04daed41cfd092df8ecc
ayarmkt <ayat.0906@gmail.com>
2021-08-14 16:59:03 +0900

    updating modal

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bugReducer from './bug-slice';
 import userReducer from './user-slice';
+import uiReducer from './ui-slice';
 
 const store = configureStore({
-  reducer: { bugs: bugReducer, user: userReducer },
+  reducer: { bugs: bugReducer, user: userReducer, ui: uiReducer },
 });
 
 export default store;
