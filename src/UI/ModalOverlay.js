b1f0a9a6db2f30b8021c1f9a2a0eb65d2cbe7656
ayarmkt <ayat.0906@gmail.com>
2021-08-14 17:03:35 +0900

    updated modal

--- src/UI/ModalOverlay.js
+++ src/UI/ModalOverlay.js
@@ -1,17 +1,25 @@
 import classes from './ModalOverlay.module.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { closeModal } from '../store/ui-slice';
 //import { createPortal } from 'react-dom';
+import { useDispatch } from 'react-redux';
 
 const Backdrop = () => {
   return <div className={classes.backdrop}></div>;
 };
 
 const Modal = () => {
+  const dispatch = useDispatch();
+
+  const closeModalHandler = () => {
+    dispatch(closeModal());
+  };
+
   return (
     <section className={classes.modal}>
       <p className={classes['modal-msg']}>Are you sure you want to delete?</p>
-      <button>Cancel</button>
+      <button onClick={closeModalHandler}>Cancel</button>
       <button>Confirm</button>
     </section>
   );
